3b8da058d6a4eb31104aefc83ee0902fec4d6485
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-23 15:05:06 -0500

    [isLoggedIn.js] Middleware setup

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const passport = require('./config/ppConfig');
 const flash = require('connect-flash');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 const app = express();
+
+// isLoggedIn middleware
+const isLoggedIn = require('./middleware/isLoggedIn');
+
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
